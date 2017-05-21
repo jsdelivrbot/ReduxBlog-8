ef6188c83c58778328e93180e02dca619573f9ed
StudentOfJS <rodlewis45@gmail.com>
2017-05-21 18:51:51 +0200

    delete, add, view

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        plugins: ['lodash'],
+        presets: ['react', 'es2015', 'stage-1', 'env']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+  
+};
